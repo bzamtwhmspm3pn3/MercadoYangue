00d71dcac4e9eee7625a8baea0cb07da3bc6034d
Vhenancio Marthinz <venanciomartinsegmail.com>
2025-09-12 23:52:33 +0100

    Corrigido erro do módulo Compra

--- backend/routes/compras.js
+++ backend/routes/compras.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Compra = require("../models/Compra");
+const Compra = require("../models/compra");
 const { authMiddleware } = require("../middlewares/auth");
 
 // Criar compra
